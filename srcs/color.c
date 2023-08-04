b095eb31a1020d2fc5a6c79fac2db362e695f18c
Jinhyeok Park <jinhyeop@c9r3s1.42seoul.kr>
2023-08-04 16:47:37 +0900

    phong shading | modify | color.c angle.c

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: jinhyeop <jinhyeop@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 00:50:37 by jinhyeop          #+#    #+#             */
-/*   Updated: 2023/08/04 06:54:18 by jinhyeop         ###   ########.fr       */
+/*   Updated: 2023/08/04 16:35:34 by jinhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,25 @@ int	diff_light(t_canvas canvas, t_ray3 *ray, double angle, int idx)
 	return (ret);
 }
 
-int	phong_light(t_canvas canvas, t_ray3 *ray, int idx)
+int	phong_light(t_canvas canvas, t_ray3 *ray, double angle, int idx)
 {
+	int	ret;
+
+	(void)ray;
+	ret = (double)canvas.light_col[idx] * canvas.light_bright \
+		* pow(angle, 0.95);
+	// printf("%lf\n", pow((angle), (double)ray->color[idx] / 255.0));
+	return (ret);
 }
 
-int	add_color(t_canvas canvas, t_ray3 *ray, double angle, int idx)
+int	add_color(t_canvas canvas, t_ray3 *ray, double *angle, int idx)
 {
 	int	color;
 
 	color = amb_light(canvas, ray, idx);
 	if (ray->type != SHADOW)
-		color = color + diff_light(canvas, ray, angle, idx) \
-			+ phong_light(canvas, ray, idx);
+		color = color + diff_light(canvas, ray, angle[0], idx) + phong_light(canvas, ray, angle[1], idx);
+		// color = color + phong_light(canvas, ray, angle[1], idx);
 	if (color > 255)
 		color = 255;
 	return (color);
@@ -49,14 +56,23 @@ int	add_color(t_canvas canvas, t_ray3 *ray, double angle, int idx)
 
 void	ray_color(t_canvas canvas, t_ray3 *ray)
 {
-	double	angle;
+	double	angle[2];
 
 	if (ray->type == SP)
-		angle = cos_sp(ray->obj, ray, canvas);
+	{
+		angle[0] = cos_sp(ray->obj, ray, canvas);
+		angle[1] = ref_sp(ray->obj, ray, canvas);
+	}
 	else if (ray-> type == PL)
-		angle = cos_pl(ray->obj, ray, canvas);
+	{
+		angle[0] = cos_pl(ray->obj, ray, canvas);
+		angle[1] = ref_pl(ray->obj, ray, canvas);
+	}
 	else
-		angle = 0.0;
+	{
+		angle[0] = 0.0;
+		angle[1] = 0.0;
+	}
 	ray->color[RED] = add_color(canvas, ray, angle, RED);
 	ray->color[GREEN] = add_color(canvas, ray, angle, GREEN);
 	ray->color[BLUE] = add_color(canvas, ray, angle, BLUE);
--- srcs/minirt.c
+++ srcs/minirt.c
@@ -6,7 +6,7 @@
 /*   By: jinhyeop <jinhyeop@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 11:48:10 by jinhyeop          #+#    #+#             */
-/*   Updated: 2023/08/04 02:58:44 by jinhyeop         ###   ########.fr       */
+/*   Updated: 2023/08/04 14:30:57 by jinhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,36 +28,6 @@ void	intersection(t_ray3 *ray, t_volume *obj, t_canvas canvas)
 		hit_plane(ray, &obj->pl[idx], canvas);
 		idx++;
 	}
-}
-
-double	cos_sp(t_sphere *sp, t_ray3 *ray, t_canvas canvas)
-{
-	t_vec3	normal;
-	t_vec3	light;
-	t_vec3	hit;
-	double	angle;
-
-	hit = add_vector(ray->origin, multiple_vector(ray->t, ray->dir));
-	normal = norm_vec(sub_vector(hit, sp->center));
-	light = norm_vec(sub_vector(canvas.light_orig, hit));
-	angle = scalar_product(normal, light);
-	if (angle < 0.0)
-		return (0.0);
-	return (angle);
-}
-
-double	cos_pl(t_plane *pl, t_ray3 *ray, t_canvas canvas)
-{
-	t_vec3	hit;
-	t_vec3	light;
-	double	angle;
-
-	hit = add_vector(ray->origin, multiple_vector(ray->t, ray->dir));
-	light = norm_vec(sub_vector(canvas.light_orig, hit));
-	angle = scalar_product(pl->norm, light);
-	if (angle < 0.0)
-		return (0.0);
-	return (angle);
 }
 
 void	color_cal(t_view *view, t_canvas canvas, t_ray3 *ray, int pix[])

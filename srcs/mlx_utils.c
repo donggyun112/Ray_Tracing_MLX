f0ae85c7b8fdb1b9525f7c4a4a0741e94283a1d0
Jinhyeok Park <jinhyeop@c3r7s3.42seoul.kr>
2023-09-11 03:36:30 +0900

    triangle write added | feat | mlx_utils.c

--- srcs/mlx_utils.c
+++ srcs/mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: jinhyeop <jinhyeop@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 12:17:38 by jinhyeop          #+#    #+#             */
-/*   Updated: 2023/09/11 02:25:46 by jinhyeop         ###   ########.fr       */
+/*   Updated: 2023/09/11 03:35:47 by jinhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -628,29 +628,55 @@ void	write_rt_cylinder(t_view *view, FILE *f)
 	}
 }
 
+void	write_rt_tpl(t_plane pl, FILE *f)
+{
+	fprintf(f, "tpl ");
+	write_rt_vec(pl.on_plane, f);
+	write_rt_vec(pl.norm, f);
+	fprintf(f, "%s \n", pl.filepath);
+}
+
+void	write_rt_cpl(t_plane pl, FILE *f)
+{
+	fprintf(f, "cpl ");
+	write_rt_vec(pl.on_plane, f);
+	write_rt_vec(pl.norm, f);
+	fprintf(f, " \n");
+}
+
+void	write_rt_pl(t_plane pl, FILE *f)
+{
+	fprintf(f, "pl ");
+	write_rt_vec(pl.on_plane, f);
+	write_rt_vec(pl.norm, f);
+	write_rt_color(pl.color, f);
+}
+
+void	write_rt_tr(t_plane pl, FILE *f)
+{
+	fprintf(f, "tr ");
+	write_rt_vec(pl.on_plane, f);
+	write_rt_vec(add_vector(pl.tr_v1, pl.on_plane), f);
+	write_rt_vec(add_vector(pl.tr_v2, pl.on_plane), f);
+	write_rt_color(pl.color, f);
+}
+
 void	write_rt_plane(t_view *view, FILE *f)
 {
-	int	i;
+	int	idx;
 
-	i = -1;
-	while (++i < view->can.obj->pl_cnt)
+	idx = 0;
+	while (idx < view->can.obj->pl_cnt)
 	{
-		if (view->can.obj->pl[i].type == NONE)
-			continue ;
-		if (view->can.obj->pl[i].type == TPL)
-			fprintf(f, "tpl ");
-		else if (view->can.obj->pl[i].type == CPL)
-			fprintf(f, "cpl ");
-		else
-			fprintf(f, "pl ");
-		write_rt_vec(view->can.obj->pl[i].on_plane, f);
-		write_rt_vec(view->can.obj->pl[i].norm, f);
-		if (view->can.obj->pl[i].type == TPL)
-			fprintf(f, "%s \n", view->can.obj->pl[i].filepath);
-		else if (view->can.obj->pl[i].type == CPL)
-			fprintf(f, " \n");
-		else
-			write_rt_color(view->can.obj->pl[i].color, f);
+		if (view->can.obj->pl[idx].type == TPL)
+			write_rt_tpl(view->can.obj->pl[idx], f);
+		else if (view->can.obj->pl[idx].type == CPL)
+			write_rt_cpl(view->can.obj->pl[idx], f);
+		else if (view->can.obj->pl[idx].type == PL)
+			write_rt_pl(view->can.obj->pl[idx], f);
+		else if (view->can.obj->pl[idx].type == TRI)
+			write_rt_tr(view->can.obj->pl[idx], f);
+		idx++;
 	}
 }
 

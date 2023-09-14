b60ec1109b236bdc0c6be5dfec08b72097e65409
Jinhyeok Park <jinhyeop@c4r1s8.42seoul.kr>
2023-09-14 09:02:18 +0900

    norminette

--- srcs/mlx_string.c
+++ srcs/mlx_string.c
@@ -6,11 +6,19 @@
 /*   By: jinhyeop <jinhyeop@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 22:21:15 by seodong-gyu       #+#    #+#             */
-/*   Updated: 2023/09/14 08:31:06 by jinhyeop         ###   ########.fr       */
+/*   Updated: 2023/09/14 08:58:33 by jinhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minirt.h"
+
+void	win_put_str(t_view *view, int line, char *str)
+{
+	const int	pos = view->mini_size + 30;
+	const int	color = 0X0FFFFF;
+
+	mlx_string_put(view->mlx, view->win, pos, line, color, str);
+}
 
 void	string_put_status(t_view *view)
 {
@@ -21,29 +29,29 @@ void	string_put_status(t_view *view)
 		tmp = ft_strjoin("Editing Mode : ", "TRUE");
 	else
 		tmp = ft_strjoin("Editing Mode : ", "FALSE");
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 100, 0XFFFFF, tmp);
+	win_put_str(view, 100, tmp);
 	free(tmp);
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 130, 0XFFFFF, "remove = F  back = G");
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 160, 0XFFFFF, "random create : Z");
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 190, 0XFFFFF, "copy : C");
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 220, 0XFFFFF, "defult rander : T");
+	win_put_str(view, 130, "Remove = F  Undo = G");
+	win_put_str(view, 160, "Random copy : Z");
+	win_put_str(view, 190, "Ropy : C");
+	win_put_str(view, 220, "Default render : T");
 	tmp2 = ft_itoa(view->quality_scalar);
-	tmp = ft_strjoin("Current Qulity : ", tmp2);
+	tmp = ft_strjoin("Current Quality : ", tmp2);
 	free(tmp2);
-	mlx_string_put(view->mlx, view->win, view->mini_size + 30, 250, 0XFFFFF, tmp);
+	win_put_str(view, 250, tmp);
 	free(tmp);
 }
 
 void	string_put_grep_status(t_view *view)
 {
 	if (view->grep.type == SP && view->click_status)
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 280, 0XFFFFF, "Grep Obj Type: SP");
+		win_put_str(view, 280, "Grep Obj Type : SP");
 	else if (view->grep.type == CY && view->click_status)
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 280, 0XFFFFF, "Grep Obj Type: CY");
+		win_put_str(view, 280, "Grep Obj Type : CY");
 	else if (view->click_status)
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 280, 0XFFFFF, "Grep Obj Type: PL");
+		win_put_str(view, 280, "Grep Obj Type : PL");
 	else
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 280, 0XFFFFF, "Grep Obj Type:   ");
+		win_put_str(view, 280, "Grep Obj Type :   ");
 }
 
 void	string_put(t_view *view)
@@ -55,15 +63,15 @@ void	string_put(t_view *view)
 	{
 		string_put_status(view);
 		string_put_grep_status(view);
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 310, 0XFFFFF, "qulity up : 1 | [ ");
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 340, 0XFFFFF, "qulity down : 2 | ] ");
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 370, 0XFFFFF, "Print ppm : 3");
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 400, 0XFFFFF, "Make .rt : 4");
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 430, 0XFFFFF, "Pause : p");
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 460, 0XFFFFF, "Mouse contol : m");
+		win_put_str(view, 310, "Quality up : 1 | [");
+		win_put_str(view, 340, "Quality down : 2 | ]");
+		win_put_str(view, 370, "Save image to ppm : 3");
+		win_put_str(view, 400, "Export .rt : 4");
+		win_put_str(view, 430, "Pause : P");
+		win_put_str(view, 460, "View mouse point : M");
 		tmp2 = ft_itoa(view->lnum);
 		tmp = ft_strjoin("lidx : ", tmp2);
-		mlx_string_put(view->mlx, view->win, view->mini_size + 30, 490, 0XFFFFF, tmp);
+		win_put_str(view, 490, tmp);
 		free(tmp);
 		free(tmp2);
 	}

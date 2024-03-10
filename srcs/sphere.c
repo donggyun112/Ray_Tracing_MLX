17241d7dc153717e6033255b657c2d8501225c92
donggyun112 <ssddgg99@daum.net>
2024-03-11 02:08:27 +0900

    f

--- srcs/sphere.c
+++ srcs/sphere.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinhyeop <jinhyeop@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seodong-gyun <seodong-gyun@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 06:40:53 by jinhyeop          #+#    #+#             */
-/*   Updated: 2023/09/13 07:33:53 by jinhyeop         ###   ########.fr       */
+/*   Updated: 2024/03/11 02:05:30 by seodong-gyu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,56 @@ void	init_sp_color(t_ray3 *ray, t_sphere *sp)
 	ray->color[BLUE] = sp->color[BLUE];
 }
 
+void	swap(float *t0, float *t1)
+{
+	float	tmp;
+
+	tmp = *t0;
+	*t0 = *t1;
+	*t1 = tmp;
+}
+
+float	get_t(float tca, float d2, float tnc, float radius)
+{
+	float	t0;
+	float	t1;
+
+	if (d2 > radius * radius)
+		return (-1.0);
+	if (tca < 0)
+	{
+		t0 = tca + tnc;
+		t1 = tca - tnc;
+	}
+	else
+	{
+		t0 = tca - tnc;
+		t1 = tca + tnc;
+	}
+	if (t0 > t1)
+		swap(&t0, &t1);
+	if (t0 < 0)
+	{
+		t0 = t1;
+		if (t0 < 0)
+			return (-1.0);
+	}
+	return (t0);
+}
+
 void	hit_sphere(t_ray3 *ray, t_sphere *sp)
 {
-	const t_vec3	l = sub_vector(sp->center, ray->origin);
-	const float		tca = scalar_product(l, ray->dir);
-	const float		d2 = scalar_product(l, l) - (tca * tca);
-	const float		tnc = sqrt(sp->radius * sp->radius - d2);
-	float			tmp;
+	const t_vec3 l = sub_vector(sp->center, ray->origin);
+	const float tca = scalar_product(l, ray->dir);
+	const float d2 = scalar_product(l, l) - (tca * tca);
+	const float tnc = sqrt(sp->radius * sp->radius - d2);
+	float t0;
 
-	if (tca < 0)
+	t0 = get_t(tca, d2, tnc, sp->radius);
+	if (t0 < 0)
 		return ;
-	if (d2 > sp->radius * sp->radius)
-		return ;
-	if (tca - tnc < 0.0)
-		tmp = tca + tnc;
-	else
-		tmp = tca - tnc;
-	if ((ray->t < 0.0 && tmp > 0.0) || (tmp > 0.0 && ray->t > tmp))
-	{
-		ray->t = tmp;
+	if ((ray->t < 0.0 && t0 > 0.0) || (t0 > 0.0 && ray->t > t0)) {
+		ray->t = t0;
 		ray->obj = (void *)sp;
 		if (sp->type == TSP || sp->type == CSP)
 			sphere_texture(ray, sp);
